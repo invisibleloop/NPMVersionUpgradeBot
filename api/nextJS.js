dc5aab6da5cbea7e14d47e8e448fe50a5e8a75cc
invisibleloop <invisibleloop@users.noreply.github.com>
2023-11-27 19:46:15 +0000

    Update nextJS.js

--- api/nextJS.js
+++ api/nextJS.js
@@ -3,28 +3,28 @@ const { kv } = require('@vercel/kv');
 
 async function handler(req, res) {
   const PACKAGE_NAME = 'next';
-  // const SLACK_TOKEN = process.env.SLACK_TOKEN;
-  // const SLACK_CHANNEL = process.env.SLACK_CHANNEL;
+  const SLACK_TOKEN = process.env.SLACK_TOKEN;
+  const SLACK_CHANNEL = process.env.SLACK_CHANNEL;
 
-  // const sendMessageToSlack = async (latestVersion) => {
-  //   const npmLink = `https://www.npmjs.com/package/next/v/${latestVersion}`;
-  //   const changelogLink = `https://github.com/vercel/next.js/releases/tag/v${latestVersion}`;
+  const sendMessageToSlack = async (latestVersion) => {
+    const npmLink = `https://www.npmjs.com/package/next/v/${latestVersion}`;
+    const changelogLink = `https://github.com/vercel/next.js/releases/tag/v${latestVersion}`;
   
-  //   const message = `New version of Next.js available: ${latestVersion}\nCheck it out on npm: ${npmLink}\nView the specific changelog: ${changelogLink}`;
+    const message = `New version of Next.js available: ${latestVersion}\nCheck it out on npm: ${npmLink}\nView the specific changelog: ${changelogLink}`;
 
-  //   try {
-  //     await axios.post('https://slack.com/api/chat.postMessage', {
-  //       channel: SLACK_CHANNEL,
-  //       text: message
-  //     }, {
-  //       headers: { Authorization: `Bearer ${SLACK_TOKEN}` }
-  //     });
-  //     console.log('Message sent to Slack:', message);
-  //   } catch (error) {
-  //     console.error('Error sending message to Slack:', error.response ? error.response.data : error.message);
-  //     res.status(500).send('Error sending message to Slack');
-  //   }
-  // };
+    try {
+      await axios.post('https://slack.com/api/chat.postMessage', {
+        channel: SLACK_CHANNEL,
+        text: message
+      }, {
+        headers: { Authorization: `Bearer ${SLACK_TOKEN}` }
+      });
+      console.log('Message sent to Slack:', message);
+    } catch (error) {
+      console.error('Error sending message to Slack:', error.response ? error.response.data : error.message);
+      res.status(500).send('Error sending message to Slack');
+    }
+  };
 
   try {
     const latestVersionResponse = await axios.get(`https://registry.npmjs.org/${PACKAGE_NAME}/latest`);
@@ -38,7 +38,7 @@ async function handler(req, res) {
       await kv.set(lastVersionKey, latestVersion);
       res.send(`New version of Next.js found and notified: ${latestVersion}`);
     } else {
-      res.send(`No new version...(${lastVersion}) Current latest version is ${latestVersion}`);
+      res.send(`No new version. Current latest version is ${latestVersion}`);
     }
   } catch (error) {
     console.error('Error:', error.message);
